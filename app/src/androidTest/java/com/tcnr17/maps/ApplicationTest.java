1c8ee7e7e6c7836624a2895f157bab07a0afd2f8
Maggie <mamamag77@gmail.com>
2015-03-22 17:35:12 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/tcnr17/maps/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.tcnr17.maps;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
